6bd58f974cbd9250da50489bf68a4f1948c45c89
MarKiE221b <mespiritu83795@liceo.edu.ph>
2023-01-27 10:53:34 +0800

    Text Change

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
               I love you Werneil Wegmann gwapo
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              A student that has nothing to do.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400"><a 
             href="https://www.facebook.com/mamark12x">
